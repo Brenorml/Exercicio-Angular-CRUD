a4c373b5c080673e5bd373fb526ecfac12e91297
Brenorml <breno.lucena@gmail.com>
2022-07-24 18:55:21 +0100

    User form created, API produced by sheet.best consumed to READ the database

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  apiUrl = 'https://sheet.best/api/sheets/dc8068ed-eafe-4f00-920b-c8415a4c8eb7';
+
+  constructor(private httpClient: HttpClient) { }
+
+  //CRUD - create, read, update, delete
+
+  //Retorna a lista de usuarios (CRUD - READ)
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.apiUrl);
+  }
+
+  //Salva usuario no banco de dados (CRUD - CREATE)
+  postUser(user: User): Observable<User> {
+    return this.httpClient.post<User>(this.apiUrl, user)
+  }
+}
