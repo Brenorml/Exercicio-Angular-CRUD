412c391d63b2be407fba9adfa5d92c52929eab64
Brenorml <breno.lucena@gmail.com>
2022-07-24 23:18:00 +0100

    Added a new route, change component list to a generic class and update form editing user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -19,17 +19,29 @@ export class UserService {
   //CRUD - create, read, update, delete
 
   //Retorna a lista de usuarios (CRUD - READ)
-  getUsers(): Observable<User[]> {
+  getUsers():Observable<User[]> {
     return this.httpClient.get<User[]>(this.apiUrl);
   }
 
   //Salva usuario no banco de dados (CRUD - CREATE)
-  postUser(user: User): Observable<User> {
+  postUser(user: User):Observable<User> {
     return this.httpClient.post<User>(this.apiUrl, user, this.httpOptions)
   }
 
   //Excluir usuario do banco de dados (CRUD - DELETE)
-  deleteUser(id: number): Observable<User> {
+  deleteUser(id: number):Observable<User> {
     return this.httpClient.delete<User>(`${this.apiUrl}/id/${id}`)
   }
+
+  //Editar usuario (CRUD - UPDATE)
+  updateUser(id: string, user: User):Observable<User> {
+    return this.httpClient.put<User>(`${this.apiUrl}/id/${id}`, user, this.httpOptions)
+  }
+
+  //Lista usuario individual
+  getUser(id: string):Observable<User[]> {
+    return this.httpClient.get<User[]>(`${this.apiUrl}/id/${id}`)
+  }
 }
+
+
